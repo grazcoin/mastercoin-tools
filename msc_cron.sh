365978f519d28a1268bc3d690d23e90ed7d1199f
Grazcoin <grazcoin@livnex.com>
2013-10-13 09:07:02 +0200

    minor cron update

--- msc_cron.sh
+++ msc_cron.sh
@@ -4,6 +4,6 @@ cd /home/dev/mastercoin-tools/
 python msc_parse.py 2>&1 > parsed.log
 python msc_validate.py 2>&1 > validated.log
 cp --no-clobber tx/* www/tx/
-cp --no-clobber addr/* www/addr/
-cp --no-clobber general/* www/general/
+cp addr/* www/addr/
+cp general/* www/general/
 python msc_archive.py 2>&1 > archived.log
