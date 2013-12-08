4660b23a37e868c158e0d46c3699d23455f5714f
Grazcoin <grazcoin@livnex.com>
2013-12-08 16:17:46 +0100

    add msc_compare_cron.sh to run comparisons

---
+++ msc_compare_cron.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+LOCK_FILE=/tmp/msc_compare_cron.lock
+COMPARE_LOG=compare.log
+
+export PATH=$PATH:/usr/local/bin/
+cd /home/dev/mastercoin-tools/
+
+# check lock (not to run multiple times)
+[ -f $LOCK_FILE ] && exit 0
+
+# lock
+touch $LOCK_FILE
+
+python msc_compare.py 2>&1 > $COMPARE_LOG
+
+# copy results to web browser directory
+cp -f general/differences.json www/general/
+
+# unlock
+rm -f $LOCK_FILE
