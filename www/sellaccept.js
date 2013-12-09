34c14b26de9a14b10f9a9aa6633d70354fe9d743
Grazcoin <grazcoin@livnex.com>
2013-12-09 19:12:17 +0100

    update icons color and popup on sellaccept

--- www/sellaccept.js
+++ www/sellaccept.js
@@ -1,10 +1,16 @@
-﻿function SellacceptController($scope, $http) {
+function SellacceptController($scope, $http, $q) {
     $scope.transactionInformation;
+    $scope.sell_offer_tx;
+    $scope.btc_payment;
     $scope.offers;
 
     $scope.footer = "FOOTER";
     $scope.title = "TITLE";
 
+    $scope.createIconPopup = function () {
+            $('.iconPopupInit').popover({ trigger: "hover" });           
+    };
+    
     $scope.getSellacceptData = function () {
 
         // parse tx from url parameters
@@ -13,25 +19,25 @@
         console.log(file);
         // Make the http request and process the result
 
-        $http.get(file, {}).success(function (data, status, headers, config) {
-            $scope.transactionInformation = data[0];
-            console.log(data);
+        $scope.tInformation = $http.get(file, {cache: false});
+	        
+	$q.all([$scope.tInformation]).then(function(values) {
+	    $scope.transactionInformation = values[0].data[0];
+	    
+	    var txfile = 'tx/' + values[0].data[0].sell_offer_txid + '.json';
+	    $scope.tInformation2 = $http.get(txfile, {cache: false});
+	    $q.all([$scope.tInformation2]).then(function(values) {
+	    	    $scope.sell_offer_tx = values[0].data[0];
+            });
+            
+            var btcpayfile = 'tx/' + values[0].data[0].tx_hash + '.json';
+            $scope.tInformation3 = $http.get(btcpayfile, {cache: false});
+	    $q.all([$scope.tInformation3]).then(function(values) {
+		    $scope.btc_payment = values[0].data[0];
+            });
         });
+        
+        
 
-       
-        var offersURL = "offers/offer-";
-        offersURL += myURLParams['tx'];
-        offersURL += ".json";
-
-        //console.log(bidsURL);
-
-        $.get(offersURL, {}).success(function (data) {
-            //  console.log(data);
-            data = $.parseJSON(data);
-            console.log(data);
-            $scope.offers = data;
-            $scope.apply();
-        });
-
-    }
+    };
 }
